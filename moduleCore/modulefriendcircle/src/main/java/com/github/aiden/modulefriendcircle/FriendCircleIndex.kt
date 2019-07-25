ccec4d56a61c60989a044db4f8ee5e45890e5aae
sunwei <tianmu19@gmail.com>
2019-07-25 18:07:42 +0800

    commit

---
+++ moduleCore/modulefriendcircle/src/main/java/com/github/aiden/modulefriendcircle/FriendCircleIndex.kt
@@ -0,0 +1,12 @@
+package com.github.aiden.modulefriendcircle
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FriendCircleIndex : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friend_circle_index)
+    }
+}
---
+++ modulex/modulebaidumap2/src/main/java/com/github/aiden/modulebaidumap2/BaidumapActivity.kt
@@ -0,0 +1,12 @@
+package com.github.aiden.modulebaidumap2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BaidumapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_baidumap)
+    }
+}
